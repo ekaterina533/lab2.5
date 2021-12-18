4d2ebeee48b1177140048eb70b8b62e142bfb8ca
ekaterina533 <ekaterinahrcnk@gmail.com>
2021-12-18 10:44:33 +0300

    change

--- задание3.py
+++ задание3.py
@@ -8,14 +8,7 @@ def min_values(**kwargs):
     min_val = min(kwargs.values())
     for k, v in kwargs.items():
         if v == min_val:
-            print(f"Переменная минимального значения: {k}, минимальное значение: {v}")
+            print("Переменная", k, "минимальное значение", v)
 
 
-min_values(
-    min_1="1",
-    min_2="2",
-    min_3="9",
-    min_4="-1",
-    min_5="2",
-    min_6="0"
-)
+min_values(a=100, b=95, c=88, d=92, e=99)
